37b1f32f011e9a16f02dc21812cf4571334a8fa0
root <root@ip-172-31-46-27.eu-north-1.compute.internal>
2024-01-18 07:11:06 +0000

    welcomepage.java file updated

--- welcomepage.java
+++ welcomepage.java
@@ -1,6 +1,6 @@
-public static void main(String args[])
-{
-        System.out.println("Hi beng");
-	System.out.println("Welcome to Java");
+class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); 
+    }
 }
 
